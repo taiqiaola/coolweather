4655da879d862d8ccfbde82cbf603c36abc21715
OK <niukai725@gmail.com>
2016-11-04 23:48:51 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -9,7 +9,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
      * Province表建表语句
      */
     public static final String CREATE_PROVINCE = "create table Province ("
-            + "id integer primary key antoincrement, "
+            + "id integer primary key autoincrement, "
             + "province_name text, "
             + "province_code text)";
 
@@ -17,7 +17,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
      * City表建表语句
      */
     public static final String CREATE_CITY = "create table City ("
-            + "id integer primary key antoincrement, "
+            + "id integer primary key autoincrement, "
             + "city_name text, "
             + "city_code text, "
             + "province_id integer)";
@@ -26,7 +26,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
      * County表建表语句
      */
     public static final String CREATE_COUNTY = "create table County ("
-            + "id integer primary key antoincrement, "
+            + "id integer primary key autoincrement, "
             + "county_name text, "
             + "county_code text, "
             + "city_id integer)";
